8d52ff56aa884ca34129ec0ae33a7162d610d6dc
ragy6581 <ravig1767>
2020-03-31 19:37:50 +0200

    Initial Commit

---
+++ MiniCalendar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MiniCalendar
+//
+//  Created by Gyani, Ravi, SKY on 31.03.20.
+//  Copyright © 2020 Gyani, Ravi, SKY. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MiniCalendar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MiniCalendar
+//
+//  Created by Gyani, Ravi, SKY on 31.03.20.
+//  Copyright © 2020 Gyani, Ravi, SKY. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
